38b80e683a5cc4baef0e7df7cb38474b6975d300
Shreeja <shreejadevu@gmail.com>
2025-01-10 09:31:34 +0530

    Add files via upload

---
+++ excelr day1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. Jupyter Notebook and Data Types
+#Write a program in Jupyter Notebook to declare variables of different data types (integer, float, string, and boolean). Print each variable and its type.
+a = 165                
+b = 10.09                
+c = "Welcome to MRU"
+d = False
+print(f"Integer: {a}, Type: {type(a)}")
+print(f"Float: {b}, Type: {type(b)}")
+print(f"String: {c}, Type: {type(c)}")
+print(f"Boolean Variable: {d}, Type: {type(d)}")
+
+
+# In[ ]:
+
+
+
+
